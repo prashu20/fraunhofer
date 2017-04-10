29ba18322b18fea0203e00c29eb91106de655796
Prashant Gupta <prashant.gupta@ipa.fraunhofer.de>
2017-04-10 14:56:51 +0200

    twit app get and post

--- bot.js
+++ bot.js
@@ -1,5 +1,17 @@
 console.log("Twitter bot is running");
 var Twit= require('twit');
-var config=require('config');
-console.log(config);
+var config=require('./config');
+
 var T = new Twit(config);
+var tweet={
+  status:'#maverick from node.js'
+}
+T.post('statuses/update', tweet, tweeted);
+function tweeted (err, data, response) {
+  if(err){
+  console.log("Something went wrong!");
+  }
+  else{
+  console.log(data);
+  }
+}
---
+++ bot1.js
@@ -0,0 +1,16 @@
+console.log("Twitter bot is running");
+var Twit= require('twit');
+var config=require('./config');
+console.log(config);
+var T = new Twit(config);
+var params={
+   q: 'Germany',
+   count: 2
+};
+
+T.get('search/tweets',params,gotdata);
+function gotdata(err,data,reponse) {
+  var tweets=data.statuses;
+  for(var i=0;i<tweets.length;i++)
+  console.log(tweets[i].text);
+}
