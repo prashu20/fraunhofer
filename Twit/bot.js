4341e51e9444140d489474fc1afb31f018eeb23a
Prashant Gupta <prashant.gupta@ipa.fraunhofer.de>
2017-04-10 13:21:48 +0200

    first step

--- Twit/bot.js
+++
@@ -1,9 +0,0 @@
-console.log("Twitter Livestream Testing");
-
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
